9fd3a512223fdb5f1c15b4ac3877107072d15f9a
transifex <transifex@linuxdeepin.com>
2018-01-22 06:49:43 +0000

    auto sync po files from transifex
    Change-Id: I9b3cd339f25f638b45181a2261eb1bc57ce78859

---
+++ deepin-font-installer/translations/deepin-font-installer_zh_CN.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>HomePage</name>
+    <message>
+        <source>Drag font file here</source>
+        <translation>拖拽字体文件到此</translation>
+    </message>
+    <message>
+        <source>Select file</source>
+        <translation>选择文件</translation>
+    </message>
+</context>
+<context>
+    <name>ListItem</name>
+    <message>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <source>Installed</source>
+        <translation>已安装</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Deepin Font Installer</source>
+        <translation>深度字体安装器</translation>
+    </message>
+    <message>
+        <source>Deepin Font Installer is used to install and uninstall font file for users with bulk install function.</source>
+        <translation>深度字体安装器用于帮助用户安装和卸载字体文件，支持批量安装。</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Deepin Font Installer</source>
+        <translation>深度字体安装器</translation>
+    </message>
+    <message>
+        <source>Bulk Install</source>
+        <translation>批量安装</translation>
+    </message>
+</context>
+<context>
+    <name>MultiFilePage</name>
+    <message>
+        <source>Install</source>
+        <translation>安装</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+</context>
+<context>
+    <name>SingleFilePage</name>
+    <message>
+        <source>Install</source>
+        <translation>安装</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>卸载</translation>
+    </message>
+    <message>
+        <source>Reinstall</source>
+        <translation>重新安装</translation>
+    </message>
+    <message>
+        <source>View font directory</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Style: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Type: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Version: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Copyright: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Description: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Same version installed</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Installed successfully</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Removed successfully</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
